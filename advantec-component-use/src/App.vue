c6e85ce2ae020fe369ae3a80aabe58b01c242e32
anilcrk <you@example.com>
2022-03-12 02:32:30 +0300

    first commit

---
+++ advantec-component-use/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6 col-md-offset-3">
+        <app-memory></app-memory>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Memory from "./Memory.vue";
+
+
+export default {
+components : {
+  appMemory : Memory
+  }
+}
+
+</script>
+
---
+++ advantec-component-use/src/Memory.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+  <p class="text-center"> bu çok güzel bir anıdır </p>
+  </div>
+</template>
+<script>
+</script>
+
+<style scoped> /* scoped --> sadece bu component de kullanıcak ise yazılır.*/
+  div {
+    border : 1px solid #cddd;
+    box-shadow: 1px 1px 2px #666;
+    padding: 20px 20px;
+    margin-top: 20px ;
+  }
+</style>
