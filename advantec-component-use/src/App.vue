6e9a6c96d62ddc8cea6da15d5841cf4d768630ee
anilcrk <you@example.com>
2022-03-13 13:59:34 +0300

    dynamic component use

--- advantec-component-use/src/App.vue
+++ advantec-component-use/src/App.vue
@@ -2,25 +2,47 @@
   <div class="container">
     <div class="row">
       <div class="col-md-6 col-md-offset-3">
-        <app-memory>
-            <h1 slot="heading">Güzek bir gün</h1>
+        <br />
+        <br />
+        <button @click="selectedComponent = 'appMemory'" class="btn-sm btn btn-primary">appMemory</button>
+        <button @click="selectedComponent = 'appNewMomory'" class="btn-sm btn btn-warning">appNewMemory</button>
+        <button @click="selectedComponent = 'appAuthor'" class="btn-sm btn btn-danger">appAuthor</button>
+        <hr />
+        <p>{{ selectedComponent }}</p>
+        <hr />
+        
+        <!-- dinamik olarak render etmek için compenent ve bind attr kullanılır.-->
+        <component :is="selectedComponent">
+          <h1 slot="heading">Güzek bir gün</h1>
+          <p slot="content">Bu gün hava karlı ve oldukça soğuk</p>
+          <p>boş slot</p>
+        </component>
+        <!-- <app-memory>
+          <h1 slot="heading">Güzek bir gün</h1>
             <p slot="content">Bu gün hava karlı ve oldukça soğuk</p>
             <p>boş slot</p>
-        </app-memory>
+        </app-memory> -->
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import Memory from "./Memory.vue";
-
+import Memory from "./Componets/Memory.vue";
+import Author from "./Componets/Author.vue";
+import NewMemory from "./Componets/NewMemory.vue";
 
 export default {
-components : {
-  appMemory : Memory
-  }
-}
-
+  components: {
+    appMemory: Memory,
+    appAuthor: Author,
+    appNewMomory: NewMemory,
+  },
+  data: function () {
+    return {
+      selectedComponent : "appMemory"
+    }
+  },
+};
 </script>
 
